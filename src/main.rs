9bd844367f2438a60651cf064694115b6cfc1d68
Kouhei.Sano <apple.x.co@gmail.com>
2025-05-16 17:57:02 +0900

    Refactor CSV record validation and improve error logging
    Reorganized loops for clarity and added line indexing for better error traceability during validation. Enhanced error logging with specific line and column details to simplify debugging.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod csv_value;
 use crate::csv_value::CsvValue;
 use anyhow::Result;
 use clap::Parser;
-use serde_json::{json, Value};
+use serde_json::{Value, json};
 use std::collections::HashMap;
 use std::fs;
 
@@ -28,26 +28,30 @@ fn main() -> Result<()> {
     //////////
     let mut reader = csv::Reader::from_path(args.csv)?;
 
-    let headers: Vec<String> = reader.headers()?
-        .iter()
-        .map(|h| h.to_string())
-        .collect();
+    let headers: Vec<String> = reader.headers()?.iter().map(|h| h.to_string()).collect();
 
     let number_of_headers = headers.len();
-    
+
+    let mut i = 0;
     for result in reader.records() {
+        i += 1;
+
         let record = result?;
 
         let mut data: HashMap<&String, CsvValue> = HashMap::new();
-        for i in 0..=number_of_headers-1 {
+        for i in 0..=number_of_headers - 1 {
             data.insert(&headers[i], CsvValue::from_str(&record[i]));
         }
         let json: Value = json!(data);
 
-        let validate_result = validator.validate(&json);
-        if !validate_result.is_ok() {
-            println!("record: {:?}", record);
-            println!("error: {:?}", validate_result.err().unwrap());
+        let result = validator.validate(&json);
+        if let Some(err) = result.err() {
+            eprintln!(
+                "line:{}, colum:{}, error:{}",
+                i,
+                &err.instance_path.to_string()[1..],
+                err.to_string()
+            );
         }
     }
 
