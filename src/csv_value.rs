1f67ae66db1b548cc00541d0e5514767b559156d
Kouhei.Sano <apple.x.co@gmail.com>
2025-05-17 08:41:29 +0900

    Add support for Null and Number types in CsvValue
    This update introduces the Null and Number variants to the CsvValue enum. Parsing and serialization logic has been updated to handle these new types. This enhances flexibility when working with diverse CSV data formats.

--- src/csv_value.rs
+++ src/csv_value.rs
@@ -3,19 +3,26 @@ use serde::Serialize;
 #[derive(Debug)]
 pub enum CsvValue {
     String(String),
+    Null,
     Integer(i64),
+    Number(f64),
     Bool(bool),
 }
 
 impl CsvValue {
     pub fn from_str(s: &str) -> Self {
-        if let Ok(num) = s.parse::<i64>() {
-            return CsvValue::Integer(num);
+        if let Ok(int) = s.parse::<i64>() {
+            return CsvValue::Integer(int);
+        }
+
+        if let Ok(num) = s.parse::<f64>() {
+            return CsvValue::Number(num);
         }
 
         match s.to_lowercase().as_str() {
             "true" => return CsvValue::Bool(true),
             "false" => return CsvValue::Bool(false),
+            "" => return CsvValue::Null,
             _ => {}
         }
         
@@ -30,7 +37,9 @@ impl Serialize for CsvValue {
     {
         match self {
             CsvValue::String(s) => serializer.serialize_str(s),
+            CsvValue::Null => serializer.serialize_none(),
             CsvValue::Integer(i) => serializer.serialize_i64(*i),
+            CsvValue::Number(n) => serializer.serialize_f64(*n),
             CsvValue::Bool(b) => serializer.serialize_bool(*b),
         }
     }
